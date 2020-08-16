0c39d4bd90a69811b795d91c1e9bad2aa4b16a26
taka603 <tkyxa603@gmail.com>
2020-08-17 08:49:26 +0900

    デザイン修正

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,11 +3,18 @@
 @section('title', 'Simple Board')
 
 @section('content')
-<form action="/posts/{{ $post->id }}">
+
+<h1>Editing Post</h1>
+<form method="POST" action="/posts/{{ $post->id }}">
     {{ csrf_field() }}
     <input type="hidden" name="_method" value="PUT">
-    <input type="text" name="title" value="{{ $post->title}}">
-    <input type="text" name="content" value="{{ $post->content}}">
-    <input type="submit">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Title</label>
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{ $post->title }}">
+    </div>
+    <button type="submit" class="btn btn-outline-primary">Submit</button>
 </form>
+
+<a href="/posts/{{ $post->id }}">Show</a>
+<a href="/posts/">Back</a>
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,17 +8,26 @@
         {{session('message')}}
     @endif
 
-<h1>Posts</h1>
+    <h1>Posts</h1>
 
-@foreach($posts as $post)
-    <a href="/post/{{ $post->id }}">{{ $post->title}}</a>
-    <a href="/posts/{{ $post->id }}/edit">Edit</a>
+    @foreach($posts as $post)
 
-    <form action="/posts/{{ $post->id}}" method="POST" onsubmit="if(confirm('Delete? Are you sure?)) { return true } else {return false };">
-        <input type="hidden" name="_method" value="DELETE">
-        <input type="hidden" name="_token" value="{{ csrf_token }}">
-        <button type="submit">Delete</button>
-    </form>
-@endforeach
+        <div class=""card>
+            <div class="card-body">
+                <h5 class="card-title">{{ $post->title }}</h5>
+                <p class="card-text">{{ $post->content }}</p>
 
-<a href="/posts/create">New Post</a>
+                <div class="d-flex" style="height: 36.4px;">
+                    <a href="/posts/{{ $post->id }}" class="btn btn-outline-primary">Show</a>
+                    <a href="/posts/{{ $post->id}}/edit" class="btn btn-outline-primary">Edit</a>
+                    <form action="/posts/{{$post->id}}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else{ return false };">
+                        <input type="hidden" name="_method" value="DELETE"> 
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+                        <button type="submit" class="btn btn-outline-danger">Delete</button> 
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    <a href="/posts/create">New Post</a>
+@endsection
