8ba2e0268c3f87c5d5289aec129abad7fac14de3
taka603 <tkyxa603@gmail.com>
2020-08-16 19:19:20 +0900

    bootstrap4セットアップ

---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token()}}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <script src="{{ asset('js/app.js) }}"></script>
+    </body>
+</html>
