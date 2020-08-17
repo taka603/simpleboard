41c68c7324d4f6576416834ee9a3328e4b1b3fcb
taka603 <tkyxa603@gmail.com>
2020-08-17 23:49:18 +0900

    ヘッダー修正・調整

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', 'PostController@index');
 Route::resource('posts', 'PostController');
 
 if(env('APP_ENV') === 'local') {
